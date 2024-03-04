b85272e98218aa401eb6d2f0b4ca43d6c65ac18a
Sachin / <sreesachin11226@gmail.com>
2024-03-04 09:00:06 +0530

    fix: add page titles for each page

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,13 +1,7 @@
-'use client'
-import AuthCard from '@/components/AuthCard'
-import Button from '@/components/ui/Button'
-import { PROJECT_NAME_TITLE_CASE } from '@/constants'
-import { redirect } from 'next/navigation'
-import React, { useRef, useState } from 'react'
-
-// export const metadata = {
-//   title: `Login to continue | ${PROJECT_NAME_TITLE_CASE}`
-// }
+"use client";
+import AuthCard from "@/components/AuthCard";
+import Button from "@/components/ui/Button";
+import React, { useRef, useState } from "react";
 
 const Login = () => {
   const mobileInputRef = useRef();
@@ -15,14 +9,29 @@ const Login = () => {
 
   return (
     <AuthCard>
-      <h2 className='font-medium text-xl text-black/90'>Let&apos;s create an account for you...</h2>
+      <h2 className='font-medium text-xl text-black/90'>
+        Let&apos;s create an account for you...
+      </h2>
       <div className='flex flex-col gap-1'>
-        <p className='font-light text-xs'>Provide your mobile number to send OTP</p>
-        <input type='text' inputMode='numeric' ref={mobileInputRef} onInput={(e) => e.currentTarget.value.length === 10? setCanSubmit(true): setCanSubmit(false) } placeholder='+91' className='w-[22rem] rounded-md bg-input px-3 py-2 focus:outline-1 focus:outline-gray-300' />
+        <p className='font-light text-xs'>
+          Provide your mobile number to send OTP
+        </p>
+        <input
+          type='text'
+          inputMode='numeric'
+          ref={mobileInputRef}
+          onInput={(e) =>
+            e.currentTarget.value.length === 10
+              ? setCanSubmit(true)
+              : setCanSubmit(false)
+          }
+          placeholder='+91'
+          className='w-[22rem] rounded-md bg-input px-3 py-2 focus:outline-1 focus:outline-gray-300'
+        />
       </div>
-      <Button label={'Get OTP'} to={'/otp'} isDisabled={!canSubmit} />
+      <Button label={"Get OTP"} to={"/otp"} isDisabled={!canSubmit} />
     </AuthCard>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
---
+++ src/app/(auth)/otp/layout.js
@@ -0,0 +1,12 @@
+import { PROJECT_NAME_TITLE_CASE } from "@/constants";
+import React from "react";
+
+export const metadata = {
+  title: PROJECT_NAME_TITLE_CASE + " | Enter OTP",
+};
+
+const Layout = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
