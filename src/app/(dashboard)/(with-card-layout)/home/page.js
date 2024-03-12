c6c0ea3d77cf80e941bbb2494e2a728347ffc0af
Sachin / <sreesachin11226@gmail.com>
2024-03-12 19:24:53 +0530

    fix(prod): remove logout + fix import

--- src/app/(dashboard)/(with-card-layout)/home/page.js
+++ src/app/(dashboard)/(with-card-layout)/home/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { LuSearch } from "react-icons/lu";
-import { UNIQUE_COURSE_NAMES } from '../../../../utils/collegeData'
+import { UNIQUE_COURSE_NAMES } from '@/utils/collegeData'
 import CollegesTable from "@/components/CollegesTable";
 
 const Home = () => {
--- src/app/api/logout/route.js
+++
@@ -1,7 +0,0 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-
-export async function GET(req) {
-    cookies().delete('actk');
-    return NextResponse.redirect(new URL('/login', req.url));
-}
