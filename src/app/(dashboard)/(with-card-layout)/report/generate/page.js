3c04dd4341a1cd5f49fdbc5ced73c5d09145c585
Sachin / <sreesachin11226@gmail.com>
2024-05-02 17:47:35 +0530

    fix: oopsie with deleted imports

--- src/app/(dashboard)/(with-card-layout)/report/generate/page.js
+++ src/app/(dashboard)/(with-card-layout)/report/generate/page.js
@@ -1,8 +1,6 @@
 "use client";
 import ReOrderTable from "@/components/ReOrderTable";
 import Button from "@/components/ui/Button";
-// import { dummyPreferenceList } from "@/utils/dummy_data";
-import { PDFExport } from "@progress/kendo-react-pdf";
 import React, { useEffect, useRef, useState } from "react";
 import { MdOutlineFileDownload } from "react-icons/md";
 import collegeData from "@/utils/collegeData";
@@ -20,8 +18,6 @@ const Generate = () => {
   const [collegPrefernces, setCollegPrefernces] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
   const [downloading, setDownloading] = useState(false);
-  const pdfComponentRef = useRef();
-  const { data } = useSession();
   const { loading, userInfo } = useUserInfo()
 
   useEffect(() => {
@@ -113,71 +109,6 @@ const Generate = () => {
         </div>
       ) : (
         <>
-          {/* <div className='fixed left-full'>
-            <PDFExport
-              ref={pdfComponentRef}
-              fileName={
-                (data?.user?.name || "User") +
-                " " +
-                new Date().toLocaleString() +
-                "-" +
-                "College Choice Report"
-              }
-              paperSize={"A4"}
-              margin='1cm'
-              creator='Get Your College'
-            >
-              <div className='flex flex-col overflow-x-scroll w-full md:overflow-x-hidden'>
-                <div className='flex w-full justify-center items-center flex-col px-4'>
-                  <a href='https://getyourcollege.com'>
-                    <Image
-                      src={"/logo_pdf.png"}
-                      width={175}
-                      alt='Get Your College Logo'
-                      height={175}
-                    />
-                  </a>
-                </div>
-                <p style={{ fontSize: "10px", marginLeft: "6px", fontWeight: "semibold" }}>Name: {userInfo?.firstName? (userInfo.firstName + ' ' + userInfo?.lastName ?? ''): 'Student'}</p>
-                <p style={{ fontSize: "10px", marginLeft: "6px" }}>Registration No.: {userInfo?.registerNo ?? 'Not given'} </p>
-                <div className='flex min-w-fit md:min-w-[unset] justify-around items-center p-2 mx-1 mt-1 md:p-4 rounded-se-lg rounded-ss-lg outline outline-1 outline-gray-200 reorder-header'>
-                  <h2 className='flex-1 font-medium min-w-16 max-w-28'>
-                    Choice Order
-                  </h2>
-                  <h2 className='flex-1 font-medium min-w-20 max-w-36'>
-                    College Code
-                  </h2>
-                  <h2 className='min-w-44 max-w-96 flex-1 font-medium md:m-0 mx-2'>
-                    College Name
-                  </h2>
-                  <h2 className='max-w-36 flex-1 font-medium min-w-32 md:m-0 mx-2'>
-                    Branch
-                  </h2>
-                </div>
-                <ReOrderTable
-                  collegPrefernces={collegPrefernces}
-                  setCollegPrefernces={setCollegPrefernces}
-                  withCutoff={false}
-                />
-              </div>
-              <div className='flex gap-1 w-full mt-5 justify-start items-center px-4'>
-                <Image src='/yt.png' width={15} height={15} alt='YT' />
-                <a
-                  href='https://www.youtube.com/c/A2KDK'
-                  style={{
-                    fontSize: "8px",
-                    textDecoration: "underline",
-                    color: "blue",
-                  }}
-                  target='_blank'
-                >
-                  Official Youtube Channel | Admission & Counselling - Guides,
-                  Updates
-                </a>
-              </div>
-            </PDFExport>
-          </div> */}
-
           <Link href='/report' className="text-blue-600 font-medium flex gap-2 items-center text-base">
             <BsChevronLeft size={14} /> Edit Colleges List
           </Link>
