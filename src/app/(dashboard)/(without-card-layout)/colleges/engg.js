0d81924c661ce7304ef16a5d904b9d2a87ce8d3a
Sachin / <sreesachin11226@gmail.com>
2024-06-13 10:03:53 +0530

    feat: page level control for ad banners

--- src/app/(dashboard)/(without-card-layout)/colleges/engg.js
+++ src/app/(dashboard)/(without-card-layout)/colleges/engg.js
@@ -34,7 +34,7 @@ const Colleges = () => {
   let subcategoryIndex = -1;
   if (currentSubCategoryType)
     subcategoryIndex = ALL_VALID_CATEGORIES.findIndex(
-      (category) => currentSubCategoryType == category.value
+      (category) => currentSubCategoryType == category.value,
     );
 
   const selectedCollegeType = useSelector((state) => state.collegeCategory);
@@ -52,26 +52,26 @@ const Colleges = () => {
 
   return (
     <>
-        <AdvertBanner />
-        <h1 className='font-medium text-2xl'>
-            Search for {selectedCollegeType} college in{" "}
-            {ALL_VALID_CATEGORIES.at(subcategoryIndex).name}
-        </h1>
-        <input
-            type='search'
-            onChange={(event) => setCollegeSearchKey(event.currentTarget.value)}
-            name='college-search'
-            id='search-input'
-            placeholder='Search by College name, College code, Branch name, District'
-            className='bg-white px-3 py-2 rounded-md outline outline-1 placeholder:text-sm outline-gray-300 focus:outline-gray-400 md:outline-gray-200 focus:outline-1 md:focus:outline-mantine-blue'
-        />
-        <ContentCard>
+      <AdvertBanner className="mt-12 md:mt-0" />
+      <h1 className="text-2xl font-medium">
+        Search for {selectedCollegeType} college in{" "}
+        {ALL_VALID_CATEGORIES.at(subcategoryIndex).name}
+      </h1>
+      <input
+        type="search"
+        onChange={(event) => setCollegeSearchKey(event.currentTarget.value)}
+        name="college-search"
+        id="search-input"
+        placeholder="Search by College name, College code, Branch name, District"
+        className="rounded-md bg-white px-3 py-2 outline outline-1 outline-gray-300 placeholder:text-sm focus:outline-1 focus:outline-gray-400 md:outline-gray-200 md:focus:outline-mantine-blue"
+      />
+      <ContentCard>
         <Suspense fallback={<SkeletonLoader rows={10} />}>
-          <div className='flex w-full justify-end items-center'>
+          <div className="flex w-full items-center justify-end">
             <SegmentedControl
               label={"Filter By"}
               value={filterBy}
-              color='blue'
+              color="blue"
               styles={{
                 root: { width: windowSize.width < 768 ? "100%" : "unset" },
               }}
--- src/components/AdvertBanner.js
+++ src/components/AdvertBanner.js
@@ -2,33 +2,56 @@
 import { DISABLE_AD_BANNER } from "@/constants";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
-import { getWindowSize } from "@/utils";
+import { cn, getWindowSize } from "@/utils";
 import { Carousel } from "@mantine/carousel";
 import "@mantine/carousel/styles.css";
 import Link from "next/link";
 import Autoplay from "embla-carousel-autoplay";
+import { usePathname } from "next/navigation";
 
 const AD_DATA = [
   {
     name: "Anna University",
     src: "/ads/ad-001.png",
     link: "https://annauniv.edu/",
+    place: "any",
+  },
+  // turn off exisiting saveetha ad
+  // {
+  //   name: "Saveetha Engineering College",
+  //   src: "/ads/ad-002.webp",
+  //   link: "https://saveetha.ac.in/",
+  // },
+  {
+    name: "Saveetha Engineering College",
+    src: "/ads/ad-005.webp",
+    link: "https://saveetha.ac.in/",
+    place: "/home",
   },
   {
     name: "Saveetha Engineering College",
-    src: "/ads/ad-002.webp",
+    src: "/ads/ad-006.webp",
     link: "https://saveetha.ac.in/",
+    place: "/colleges",
+  },
+  {
+    name: "Saveetha Engineering College",
+    src: "/ads/ad-004.webp",
+    link: "https://saveetha.ac.in/",
+    place: "/discover",
   },
   {
     name: "Sairam Engineering College",
     src: "/ads/ad-003.webp",
     link: "https://www.sairam.edu.in",
+    place: "any",
   },
 ];
 
-function AdvertBanner() {
+function AdvertBanner({ className }) {
   const [windowSize, setWindowSize] = useState({ width: 1453, height: 1234 });
   const autoplay = useRef(Autoplay({ delay: 4000 }));
+  const location = usePathname();
 
   useEffect(() => {
     setWindowSize(getWindowSize());
@@ -45,20 +68,43 @@ function AdvertBanner() {
       loop
       plugins={[autoplay.current]}
       withIndicators
+      className={cn(className)}
     >
-      {AD_DATA.map((ad, i) => (
-        <Carousel.Slide key={i} className="flex w-full justify-center">
-          <Link href={ad.link} target="_blank" className="flex justify-center">
-            <Image
-              alt={ad.name}
-              width={756}
-              height={windowSize.width < 768 ? 100 : 55}
-              src={ad.src}
-              className="object-contain"
-            />
-          </Link>
-        </Carousel.Slide>
-      ))}
+      {AD_DATA.map((ad, i) =>
+        !ad.place || ad.place === "any" ? (
+          <Carousel.Slide key={i} className="flex w-full justify-center">
+            <Link
+              href={ad.link}
+              target="_blank"
+              className="flex justify-center"
+            >
+              <Image
+                alt={ad.name}
+                width={756}
+                height={windowSize.width < 768 ? 100 : 55}
+                src={ad.src}
+                className="object-contain"
+              />
+            </Link>
+          </Carousel.Slide>
+        ) : ad.place === location ? (
+          <Carousel.Slide key={i} className="flex w-full justify-center">
+            <Link
+              href={ad.link}
+              target="_blank"
+              className="flex justify-center"
+            >
+              <Image
+                alt={ad.name}
+                width={756}
+                height={windowSize.width < 768 ? 100 : 55}
+                src={ad.src}
+                className="object-contain"
+              />
+            </Link>
+          </Carousel.Slide>
+        ) : null,
+      )}
     </Carousel>
   ) : null;
 }
